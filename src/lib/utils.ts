6ec282713d24b3e68ba59a0e767301e34188c79d
Harshil Patel <harshilp046@gmail.com>
2026-06-18 11:05:46 +0530

    feat: add delay fn. to check skeleton loaders, must use with await

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Set custom delay
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
